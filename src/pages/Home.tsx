b11313317dcf6a87420ad430c3f54db82514eb1d
ItaloCabral <g1brunoyokima@hotmail.com>
2022-05-24 23:48:27 -0300

    Initial Commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Container, Title } from "../styles/Home";
+
+import UserCard from "../components/UserCard";
+
+const Home: React.FC = () => {
+
+  interface User{
+    id: number;
+    name: string;
+    img: string;
+    username: string;
+  }
+
+  const [users, setUsers] = useState<User[]>([]);
+  // USERS_ENDPOINT=
+  // TRANSACTIONS_ENDPOINT=https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989
+  useEffect(() => {
+    async function loadUsers(){
+      const response = await fetch('https://www.mocky.io/v2/5d531c4f2e0000620081ddce');
+      const data = await response.json();
+      setUsers(data);
+    }
+    loadUsers();
+  }, []);
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
+  return (
+    <Container>
+      <Title>Olá mundo</Title>
+      {
+        users.map(user => 
+          <UserCard
+            id={user.id}
+            name={user.name}
+            img={user.img}
+            username={user.username}
+          />
+        )
+      }
+    </Container>
+  );
+}
+
+export default Home;
---
+++ src/styles/Home.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  background: #f0f0f5;
+`;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+`;
