d1129a75549053b294c25ce446fafbfab9f0076c
ItaloCabral <g1brunoyokima@hotmail.com>
2022-05-28 18:34:09 -0300

    Reestruturação da Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { Container, Title } from "../styles/Home";
+import React, { useEffect } from "react";
+import { Container, Title } from "styles/HomeStyles";
 
-import UserCard from "../components/UserCard";
+import { useUser } from "hooks";
+
+import { UserCard } from "components";
 
 export const Home: React.FC = () => {
 
-  interface User{
-    id: number;
-    name: string;
-    img: string;
-    username: string;
-  }
-
-  const [users, setUsers] = useState<User[]>([]);
-  // USERS_ENDPOINT=
-  // TRANSACTIONS_ENDPOINT=https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989
-  useEffect(() => {
-    async function loadUsers(){
-      const response = await fetch('https://www.mocky.io/v2/5d531c4f2e0000620081ddce');
-      const data = await response.json();
-      setUsers(data);
-    }
-    loadUsers();
-  }, []);
+  const { users, getAll, loading } = useUser();
 
   useEffect(() => {
-    console.log(users);
-  }, [users]);
+    console.log(loading);
+    getAll();
+  }, [getAll]);
 
   return (
     <Container>
       <Title>Lista de Usuários</Title>
       {
-        users.map(user => 
-          <UserCard
-            key={user.id}
-            id={user.id}
-            name={user.name}
-            img={user.img}
-            username={user.username}
-          />
-        )
+        loading ? (
+          <p>Carregando...</p>
+        ) : (
+          users.map(user => 
+            <UserCard
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              img={user.img}
+              username={user.username}
+            />
+          ))
       }
     </Container>
   );
