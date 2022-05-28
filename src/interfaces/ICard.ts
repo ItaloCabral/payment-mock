1b12bd407089bee25ba15fb2f847e64711618048
ItaloCabral <g1brunoyokima@hotmail.com>
2022-05-28 18:33:43 -0300

    Card, Services e Interfaces

---
+++ src/interfaces/ICard.ts
@@ -0,0 +1,5 @@
+export interface ICard {
+  cardNumber: string;
+  cvv: number;
+  expiryDate: string;
+}
---
+++ src/services/CardsService.ts
@@ -0,0 +1,20 @@
+import { ICard } from "../interfaces";
+
+const cardList = [
+  {
+    cardNumber: '1111111111111111',
+    cvv: 111,
+    expiryDate: '01/18',
+  },
+  {
+    cardNumber: '4111111111111234',
+    cvv: 123,
+    expiryDate: '01/20',
+  },
+];
+
+const getCards = (): ICard[] => cardList;
+
+export const CardService = {
+  getCards
+}
