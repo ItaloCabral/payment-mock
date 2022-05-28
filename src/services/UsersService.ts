48e467db56c2c24c76cb778799bd083ed2deeb01
ItaloCabral <g1brunoyokima@hotmail.com>
2022-05-28 15:43:41 -0300

    User Service

---
+++ src/services/UsersService.ts
@@ -0,0 +1,8 @@
+import { Api } from '../providers';
+import { IUser } from '../interfaces';
+
+const getAllUsers = async () => Api.get<IUser[]>("/v2/5d531c4f2e0000620081ddce");
+
+export const UsersService = {
+  getAllUsers
+};
