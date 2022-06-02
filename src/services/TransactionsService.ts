b8a363c9f1f39c90e8db240bc25a777a14325cb7
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-02 09:42:59 -0300

    Transaction Service Fix

--- src/services/TransactionsService.ts
+++ src/services/TransactionsService.ts
@@ -1,5 +1,5 @@
 import { Api } from 'providers';
-import { ITransaction, ITransactionPayload } from '../interfaces';
+import { ITransaction, ITransactionPayload } from 'interfaces';
 
 const createTransaction = (payload: ITransactionPayload) => Api.post<ITransaction>('/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989', payload);
 
