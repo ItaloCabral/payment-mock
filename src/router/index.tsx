ebf386e54f85270258c3f6c3c322e3d77a0b31f0
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-05 18:05:42 -0300

    Configs to Deploy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,7 +8,7 @@ import {
 import { Home } from 'pages';
 
 export const Router = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate to="/" />} />
