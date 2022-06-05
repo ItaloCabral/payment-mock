4d39702a9bcf8ebe4c1cb04ab9790fee286ab561
ItaloCabral <g1brunoyokima@hotmail.com>
2022-06-05 17:52:45 -0300

    Amount Context

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import { UserProvider } from "./UserContext";
 import { CardProvider } from "./CardContext";
+import { AmountProvider } from "./AmountContext";
 
 export const GlobalProvider: React.FC<{children: React.ReactNode}> = ({ children }) => {
   return (
     <UserProvider>
       <CardProvider>
-        {children}
+        <AmountProvider>
+          {children}
+        </AmountProvider>
       </CardProvider>
     </UserProvider>
   );
